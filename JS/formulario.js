32331cedc4aa17650b0647b704a51b36c88ae65b
Jana Fisdel <janafisdel@gmail.com>
2022-01-18 16:10:50 -0300

    conexion a formulario con base de datos

---
+++ JS/formulario.js
@@ -0,0 +1,28 @@
+//FORMULARIO DE COMENTARIOS
+// function botonFormulario(){
+//     let nombre = document.getElementById("nombre").value
+//     let email =	document.getElementById("email").value
+//     let  serie = document.getElementById("serie").value
+   		
+    
+
+//     let enviar = document.getElementById("botonEnviar");
+   
+//     if (nombre === "" || email === "" || serie === ""){
+//         let send = document.createElement("div")
+//         send.innerHTML =""
+//        send.innerHTML=`<input type="button" id="boton" value="Enviar"  disabled>
+//        `
+      
+//        enviar.appendChild(send)
+
+//     } else {
+        
+//         let send = document.createElement("div")
+//         send.innerHTML =""
+//        send.innerHTML=`<input type="button" id="boton" value="Enviar" onclick="nuevodato();">
+//        `
+      
+//        enviar.appendChild(send)
+// }}
+// botonFormulario()
---
+++ conectarBaseDatos.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+from flask import jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+app.config['MYSQL_HOST'] = 'cursophp.com.ar'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'fullstack'
+app.config['MYSQL_DB'] = 'FisdelJana'
+mysql = MySQL(app)
+
+			
+		
+
+@app.route('/nuevo', methods=['POST'])	
+@cross_origin()	
+def nuevo():
+		if request.method == 'POST':
+			request_data = request.get_json()
+			nombre = request_data['nombre']
+			email =request_data['email']
+			serie=request_data['serie']
+			comentario=request_data['comentario']
+			cur = mysql.connection.cursor()
+			cur.execute('INSERT INTO contacto (nombre,email,serie,comentario) VALUES (%s, %s, %s,%s)', (nombre,email,serie,comentario) )			
+			mysql.connection.commit()
+			return "Guardado OK"
+					
+			
+if __name__	== '__main__':
+			app.run(port=4000, debug=True)
+			
+
